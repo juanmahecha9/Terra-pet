d60cba23ed25a4884d02c418885d7984da0772c2
arleycas <eyhsoncastro@gmail.com>
2020-06-01 11:59:26 -0500

    frontend arley

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EquipoComponent } from './equipo/equipo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquipoComponent
   ],
   imports: [
     BrowserModule,
