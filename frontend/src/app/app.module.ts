3c6b4b2620a8a9a3daf938c415628bb02988c8ea
laurAlarcon <lauravalentinalarcon@gmail.com>
2020-06-02 10:13:13 -0500

    componentes login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EquipoComponent } from './equipo/equipo.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EquipoComponent
+    EquipoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
