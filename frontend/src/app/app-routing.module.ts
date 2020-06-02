68f4e9c4d698e59788944104822ff0e150a10e32
Users <user@Userss-iMac.local>
2020-06-02 11:12:18 -0500

    rutas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 /* importar componentes */
-
+import { EquipoComponent } from './componentes/equipo/equipo.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
 const routes: Routes = [
- 
+  {
+
+    path: 'team',
+    component: EquipoComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'regitro',
+    component: RegistroComponent
+  }
 ];
 
 @NgModule({
