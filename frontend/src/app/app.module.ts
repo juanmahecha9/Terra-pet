02d3b11be5185c2c4f741966a3717ade0ccfc4af
mariayveth <mariayvethramos@gmail.com>
2020-06-02 12:04:20 -0500

    creacionnosotros

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { EquipoComponent } from './componentes/equipo/equipo.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { InicioComponent } from './componentes/inicio/inicio.component';
     EquipoComponent,
     LoginComponent,
     RegistroComponent,
-    InicioComponent
+    InicioComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
