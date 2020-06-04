93df484cc95c638287f9f49a52eb38904f7bb680
Users <user@Userss-iMac.local>
2020-06-04 16:36:20 -0500

    cambio variable

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-  private URL = 'http://localhost:8001/api';
+  private URL = 'http://localhost:8000/api';
   constructor(private http: HttpClient, private router: Router) { }
 
   signUpUser(user) {
